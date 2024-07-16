c5c2396e7d96f6b2d0fda31c1d90d057870954c8
JiunHongLim <xavier4666@gmail.com>
2024-07-17 03:39:43 +0800

    Solved epoch <1 problem

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(r'C:\Users\xavie\OneDrive\Documents\Y3S2\Y3S2 Machine '
                                                  r'Learning\real_vs_fake\real-vs-fake\train',
-                                                 target_size=(64, 64),
-                                                 batch_size=32,
+                                                 target_size=(32, 32),
+                                                 batch_size=500,
                                                  class_mode='binary')
 
 validation_set = test_datagen.flow_from_directory(r'C:\Users\xavie\OneDrive\Documents\Y3S2\Y3S2 Machine '
                                                   r'Learning\real_vs_fake\real-vs-fake\valid',
-                                                  target_size=(64, 64),
-                                                  batch_size=32,
+                                                  target_size=(32, 32),
+                                                  batch_size=100,
                                                   class_mode='binary')
 
 # Model creation
 model = Sequential()
 
 # Adding layers
-model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3), activation='relu'))
@@ -40,16 +40,11 @@ model.add(Dense(units=1, activation='sigmoid'))
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Calculate steps per epoch
-steps_per_epoch = training_set.samples // training_set.batch_size
-validation_steps = validation_set.samples // validation_set.batch_size
-
 # Train the model
 history = model.fit(training_set,
-                    steps_per_epoch=steps_per_epoch,
-                    epochs=1,
+                    epochs=100,
                     validation_data=validation_set,
-                    validation_steps=validation_steps)
+                    )
 
 # Evaluate the model
 validation_loss, validation_accuracy = model.evaluate(validation_set)
@@ -65,22 +60,19 @@ print(confusion_matrix(validation_set.classes, y_pred))
 # Classification report
 print(classification_report(validation_set.classes, y_pred, target_names=['Real', 'Fake']))
 
-# Plot training & validation accuracy values
-plt.figure(figsize=(12, 4))
-plt.subplot(1, 2, 1)
+# Visualizations
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.xlabel('Epoch')
+plt.title('Model Accuracy (training & valid)')
 plt.ylabel('Accuracy')
-plt.legend(['Train', 'Validation'], loc='upper left')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Valid'], loc='upper right')
+plt.show()
 
-# Plot training & validation loss values
-plt.subplot(1, 2, 2)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.xlabel('Epoch')
+plt.title('Model Loss (training & val)')
 plt.ylabel('Loss')
-plt.legend(['Train', 'Validation'], loc='upper left')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Valid'], loc='upper right')
 plt.show()
